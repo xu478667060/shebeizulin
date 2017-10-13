95aba349f4099fc191c6498a890aa7129755f798
xu <478667060@qq.com>
2017-10-13 14:03:15 +0800

    详情页基本样式

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -5,7 +5,6 @@ import {CommonModule} from "@angular/common";
 import {NgZorroAntdModule} from "ng-zorro-antd";
 import {DownSelectComponent} from "../component/down-select/down-select.component";
 import {RouterModule} from "@angular/router";
-import {DeviceDetailComponent} from "../feature/details/device-detail/device-detail.component";
 import {SearchBoxComponent} from "../component/search-box/search-box.component";
 import {TableListComponent} from "../component/table-list/table-list.component";
 import {DivPageComponent} from "../component/div-page/div-page.component";
