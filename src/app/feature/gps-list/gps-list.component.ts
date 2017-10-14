ebfb734b5d61853bf437b9177e9889475c0041ac
cwl木子 <1159849884@qq.com>
2017-10-14 10:05:18 +0800

    冲突解决

--- src/app/feature/gps-list/gps-list.component.ts
+++ src/app/feature/gps-list/gps-list.component.ts
@@ -7,9 +7,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GpsListComponent implements OnInit {
 
+  searchTip = "请输入GPS编号"
+
+  th: Array<any> = [
+    {name: "GPS编号", type: "big-large-more"},
+    {name: "坐标", type: "big-large-more"},
+    {name: "绑定终端", type: "big-large-more"}
+  ]
+
+  dataList = [
+    [
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ], [
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ], [
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ], [
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ], [
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ], [
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ], [
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ], [
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ], [
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ],[
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ],[
+      {name: "46546465465", type: "big-large-more"},
+      {name: "116.527301,39.94083", type: "big-large-more"},
+      {name: "46546465454645", type: "big-large-more"},
+    ],
+  ]
+
+  hasOperation:boolean = false
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // TODO:搜索 筛选列表
+  search(keyWord: string) {
+    console.log(keyWord)
+  }
+
+  // TODO:分页查询
+  switchPage(pageNum){
+    console.log(pageNum)
   }
 
 }
--- src/app/feature/maintainer/maintainer.component.ts
+++ src/app/feature/maintainer/maintainer.component.ts
@@ -7,6 +7,113 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaintainerComponent implements OnInit {
 
+
+  searchTip = "请输入维修员名称"
+
+  th: Array<any> = [
+    {name: "姓名", type: "big-large"},
+    {name: "联系方式", type: "large"},
+    {name: "技术类型", type: "small"},
+    {name: "可服务品牌", type: "big-large"},
+    {name: "维修类型", type: "small"},
+    {name: "申请时间", type: "large"},
+    {name: "操作", type: "flex"},
+  ]
+
+  dataList = [
+    [
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ], [
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ], [
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ], [
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ], [
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ], [
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ], [
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ],[
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ],[
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ],[
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ],[
+      {name: "1231313132", type: "small"},
+      {name: "454646545465645", type: "large"},
+      {name: "启动", type: "small"},
+      {name: "在线", type: "small"},
+      {name: "杭州西湖区华彩国际店", type: "large"},
+      {name: "1000", type: "small"},
+      {name: "发电机组01", type: "large"},
+    ],
+  ]
+
+  operationType = "terminal"
+
   constructor() { }
 
   ngOnInit() {
